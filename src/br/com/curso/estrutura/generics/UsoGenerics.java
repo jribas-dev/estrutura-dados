f0dc952ffe408bad857a79dd69cccfda26fa1fba
jribas-dev <3b3brazil@gmail.com>
2023-02-24 15:24:37 -0300

    alguns comentários sobre Generics

--- src/br/com/curso/estrutura/generics/UsoGenerics.java
+++ src/br/com/curso/estrutura/generics/UsoGenerics.java
@@ -1,5 +1,15 @@
 package br.com.curso.estrutura.generics;
 
+// Unknown Wildcard			=> <>
+// UpperBounded Wildcard	=> <? extends Pessoa>
+// LowerBounded Wildcard	=> <? super Pessoa>
+
+// K para Key: 		Map<K,V>
+// V para Value:	Map<K,V>
+// E para Element:	List<E>
+// T para Type:		Collections#addAll
+// ? quando generico
+
 public class UsoGenerics<T> {
 	private T t;
 
